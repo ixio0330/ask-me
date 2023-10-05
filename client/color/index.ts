4b3c1b773cae660eeeca5e591c8c41529b448143
ixio0330 <ixio0330@gmail.com>
2023-10-05 23:57:33 +0900

    🚧 컬러 팔레트 수정

--- client/color/index.ts
+++ client/color/index.ts
@@ -1,9 +1,29 @@
+const palette = {
+  global: {
+    black: '#000',
+    white: '#fff',
+    light: '#F6F6F6',
+  }
+};
+
+const theme = {
+  light: {
+    primary: palette.global.black,
+    secondary: 'rgba(0,0,0,0.4)',
+    background: palette.global.light,
+    card: palette.global.white,
+    border: '#E2E2E2',
+  },
+  dark: {},
+}
+
 const color = {
-  primary: '#3182CE',
   tertiary: '#EDF1F5',
   white: '#fff',
   black: '#000',
-  gray: '#8E8E8E'
+  light: '#F6F6F6',
+  gray: '#8E8E8E',
+  ...theme.light,
 };
 
 export default color;
