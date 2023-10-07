208f340504e71a5b9cd63e9ca99e51d3b762ac6f
ixio0330 <ixio0330@gmail.com>
2023-10-07 19:45:17 +0900

    ✨ Bio 컴포넌트

--- client/components/Card/Bio.tsx
+++ client/components/Card/Bio.tsx
@@ -1,19 +1,106 @@
 import styled from "@emotion/styled";
-import { ComponentProps } from "react";
 import color from "@/client/color";
 import Input from "../Input";
 import Button from "../Button";
+import Typography from "../Typography";
+import { InAuthUser } from "@/common/models/in_auth_user";
+import Image from 'next/image'
+import { ComponentProps } from "react";
 
-const Bio = () => {
+export type BioStatus = 'visitor' | 'owner' | 'update';
+
+export interface BioProps extends Pick<InAuthUser, 'displayName' | 'photoURL'>, ComponentProps<'input'> {
+  status?: BioStatus;
+  bio: string;
+  onClickSend?: () => void;
+  onClickUpdateBio?: () => void;
+  onClickSaveBio?: () => void;
+  onClickCancel?: () => void;
+}
+
+const Bio = (
+  { 
+    status = 'visitor', 
+    bio,
+    photoURL, 
+    displayName, 
+    value, 
+    placeholder, 
+    onChange, 
+    onClickSend,
+    onClickUpdateBio,
+    onClickSaveBio,
+    onClickCancel,
+  }: BioProps
+) => {
+
+  const render = () => {
+    switch (status) {
+      case 'visitor':
+        return (
+          <>
+            <Typography>{bio}</Typography>
+            <Input 
+              placeholder={placeholder ?? '무엇이 궁금한가요?'}
+              value={value}
+              onChange={onChange} 
+            />
+            <Button 
+              onClick={onClickSend}
+              style={{ width: 'auto', justifySelf: 'end' }}
+            >
+              등록
+            </Button>
+          </>
+        );
+      case 'owner':
+        return (
+          <>
+            <Typography style={{ height: 46, lineHeight: '46px' }}>{bio}</Typography>
+            <Button 
+              onClick={onClickUpdateBio}
+              style={{ width: 'auto', justifySelf: 'end' }}
+            >
+              소개 수정
+            </Button>
+          </>
+        )
+      case 'update':
+        return (
+          <>
+            <Input 
+              placeholder={placeholder ?? '소개를 입력해주세요'}
+              value={value}
+              onChange={onChange}
+            />
+            <S.BioUpdateActions>
+              <Button 
+                variant='outlined'
+                onClick={onClickCancel}
+              >
+                취소
+              </Button>
+              <Button onClick={onClickSaveBio}>
+                소개 저장
+              </Button>
+            </S.BioUpdateActions>
+          </>
+        );
+    }
+  };
+
   return (
     <S.Bio>
       <S.UserProfile>
-        <S.UserProfileImage />
-        서나무
+        <S.UserProfileImage 
+          src={photoURL ?? ''} 
+          width={60} 
+          height={60} 
+          alt={`${displayName} 프로필 이미지`} 
+        />
+        <Typography size='l' weight='b'>{displayName}</Typography>
       </S.UserProfile>
-      <p>안녕하세요, 서나무입니다.</p>
-      <Input placeholder='무엇이 궁금한가요?' />
-      <Button>등록</Button>
+      {render()}
     </S.Bio>
   )
 };
@@ -32,14 +119,19 @@ const S = {
     padding: 20px;
     box-sizing: border-box;
   `,
-  UserProfileImage: styled('img')`
+  UserProfileImage: styled(Image)`
     margin-right: 12px;
+    border-radius: 50%;
   `,
   UserProfile: styled('div')`
     display: flex;
-    justify-content: center;
     align-items: center;
   `,
-};
+  BioUpdateActions: styled('div')`
+    justify-self: end;
 
-// https://lh3.googleusercontent.com/a/AEdFTp4B9If0zw6ex3tT7TYldHLlMV5Y5QIKnRhcrJiv=s96-c
+    & button {
+      margin-left: 12px;
+    }
+  `,
+};
--- stories/Bio.stories.tsx
+++ stories/Bio.stories.tsx
@@ -1,5 +1,6 @@
-import Bio from '@/client/components/Card/Bio';
+import Bio, { BioProps, BioStatus } from '@/client/components/Card/Bio';
 import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
 
 const meta: Meta<typeof Bio> = {
   title: 'components/Card/Bio',
@@ -11,13 +12,41 @@ type Story = StoryObj<typeof Bio>;
 
 export const Default: Story = {
   args: {
-    children: '버튼',
+    photoURL: 'https://lh3.googleusercontent.com/a/AEdFTp4B9If0zw6ex3tT7TYldHLlMV5Y5QIKnRhcrJiv=s96-c',
+    displayName: '서나무',
+    bio: '안녕하세요, 서나무입니다'
   }
 };
 
-export const Outlined: Story = {
-  args: {
-    children: '버튼',
-    variant: 'outlined'
-  }
+const BioWithHooks = () => {
+  const [bio, setBio] = useState('안녕하세요, 서나무입니다');
+  const [newBio, setNewBio] = useState(bio);
+  const [status, setStatus] = useState<BioStatus>('owner');
+  const onClickUpdateBio = () => setStatus('update');
+  const onClickCancel = () => {
+    setStatus('owner');
+    setNewBio(bio);
+  };
+  const onClickSaveBio = () => {
+    setBio(newBio);
+    setStatus('owner');
+  };
+
+  return (
+    <Bio 
+      photoURL='https://lh3.googleusercontent.com/a/AEdFTp4B9If0zw6ex3tT7TYldHLlMV5Y5QIKnRhcrJiv=s96-c'
+      displayName='서나무'
+      status={status} 
+      bio={bio} 
+      onClickUpdateBio={onClickUpdateBio}
+      onClickCancel={onClickCancel}
+      onClickSaveBio={onClickSaveBio}
+      value={newBio}
+      onChange={e => setNewBio(e.target.value)}
+    />
+  )
+};
+
+export const Owner: Story = {
+  render: (props) => <BioWithHooks {...props} />,
 };
