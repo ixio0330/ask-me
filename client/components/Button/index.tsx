8f348cb71f7a363ca84cefdb2f6173cc25628913
ixio0330 <ixio0330@gmail.com>
2023-10-07 14:01:16 +0900

    ✨ Button 컴포넌트

---
+++ client/components/Button/index.tsx
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+import { ComponentProps } from "react";
+import color from "@/client/color";
+
+interface ButtonProps extends ComponentProps<'button'> {
+  variant: 'fill' | 'outlined'
+}
+
+const Button = ({ children, variant = 'fill' }: ButtonProps) => {
+  return <S.Button variant={variant}>{children}</S.Button>
+};
+
+export default Button;
+
+const S = {
+  Button: styled('button')<ButtonProps>`
+    display: inline-flex;
+    padding: 12px 24px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    background-color: ${({ variant }) => variant === 'fill' ? color.primary : color.background};
+    color: ${({ variant }) => variant === 'fill' ? color.background : color.primary};
+    border: 1px solid ${color.primary};
+    font-weight: bold;
+    border-radius: 4px;
+  `,
+};
