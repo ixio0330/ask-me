d2f671ac2ec39d0fa6abbf44a9917f6a256b6d29
ixio0330 <ixio0330@gmail.com>
2023-10-07 22:36:10 +0900

    ✨ Reply 컴포넌트 분리

---
+++ client/components/Typography/Reply.tsx
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+import color from "@/client/color";
+import Typography, { TypographyProps } from ".";
+
+const Reply = ({ children }: TypographyProps) => {
+  return <S.Reply size='s'>{children}</S.Reply>
+};
+
+export default Reply;
+
+const S = {
+  Reply: styled(Typography)`
+    margin: 0;
+    border-left: 4px solid ${color.primary};
+    padding-left: 16px;
+    margin-top: 4px;
+  `,
+};
