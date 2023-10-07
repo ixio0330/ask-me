9946193f531a5a1f16a92580b36c642d4e7b167f
ixio0330 <ixio0330@gmail.com>
2023-10-07 11:21:31 +0900

    👽️ 질문 API 마이그레이션

--- app/api/ask/route.ts
+++ app/api/ask/route.ts
@@ -1,7 +1,8 @@
 import { responseErrorHandler } from '@/server/middleware/errorHandler';
 import { NextRequest, NextResponse } from 'next/server';
-import { BadRequest, InternalServerError } from '@/server/error';
+import { BadRequest, InternalServerError, NotValidToken } from '@/server/error';
 import AskStorage from '@/server/storage/ask';
+import firebaseAdmin from '@/common/firebase/admin';
 
 export async function GET(req: NextRequest) {
   try {
@@ -32,6 +33,69 @@ export async function GET(req: NextRequest) {
   }
 }
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const { uid, ask } = body;
   
+    if (uid === undefined || uid === null) {
+      throw new BadRequest('uid가 누락되었습니다.');
+    }
+  
+    if (ask === undefined || ask === null) {
+      throw new BadRequest('ask가 누락되었습니다.');
+    }
+  
+    const addResult = await AskStorage.add(body);
+    if (!addResult.result) {
+      throw new InternalServerError(addResult.message);
+    }
+    return Response.json(addResult, { status: 200 });
+  } catch(error) {
+    return responseErrorHandler(error);
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  try {
+    const token = req.headers.get('authorization');
+    if (token === null) {
+      throw new NotValidToken('권한이 없습니다.');
+    }
+
+    let tokenUid: null | string = null;
+    try {
+      const decode = await firebaseAdmin.Auth.verifyIdToken(token);
+      tokenUid = decode.uid;
+    } catch (error) {
+      throw new NotValidToken();
+    }
+
+    const body = await req.json();
+    const { uid, askId, deny } = body;
+
+    if (uid !== tokenUid) {
+      throw new BadRequest('수정 권한이 없습니다.');
+    }
+
+    if (uid === undefined || uid === null) {
+      throw new BadRequest('uid가 누락되었습니다.');
+    }
+  
+    if (askId === undefined || askId === null) {
+      throw new BadRequest('askId가 누락되었습니다.');
+    }
+
+    if (deny === undefined || deny === null) {
+      throw new BadRequest('deny가 누락되었습니다.');
+    }
+
+    const addResult = await AskStorage.updateAskStatus(body);
+    if (!addResult.result) {
+      throw new InternalServerError(addResult.message);
+    }
+    return addResult.data;
+  } catch(error) {
+    return responseErrorHandler(error);
+  }
 }
