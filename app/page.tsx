c6f2ef2e6b8031664ec4bcc64daa057b008bf4db
ixio0330 <ixio0330@gmail.com>
2023-10-08 14:04:35 +0900

    💄 Chakra UI 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { NextPage } from 'next';
-import { Box, Heading, Flex, Text } from '@chakra-ui/react';
 import GoogleLoginButton from '@/client/components/Button/GoogleLoginButton';
 import { useAuth } from '@/client/context/auth_user';
 import AppLayout from '@/client/components/App/Layout';
+import Image from 'next/image';
+import Stack from '@/client/components/Stack';
+import Typography from '@/client/components/Typography';
 
 const IndexPage: NextPage = () => {
   const { signInWithGoogle } = useAuth();
@@ -13,21 +15,18 @@ const IndexPage: NextPage = () => {
     <AppLayout 
       title='Ask Me'
     >
-      <Box maxW='md' mt='20'>
-        <img src='/logo.svg' alt='로고' />
-        <Flex 
-          align='center'
-          justify='center' 
-          direction='column'
-          mt='10'
-        >
-          <Heading>Ask Me</Heading>
-          <Text mt='2'>궁금한 것이 있으면 물어보세요!</Text>
-        </Flex>
-      </Box>
-      <Box mt='20'>
+      <Stack style={{ marginTop: 100, textAlign: 'center' }}>
+        <Image 
+          style={{ margin: '0 auto' }} 
+          src='/logo.svg' 
+          alt='로고' 
+          width={320} 
+          height={320} 
+        />
+        <Typography size='l' weight='b'>Ask Me</Typography>
+        <Typography>궁금한 것이 있으면 물어보세요!</Typography>
         <GoogleLoginButton onClick={signInWithGoogle}>Google로 시작하기</GoogleLoginButton>
-      </Box>
+      </Stack>
     </AppLayout>
   )
 }
