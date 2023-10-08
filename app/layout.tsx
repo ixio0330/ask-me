d1be09f2a7e192a91529bb5fb2ebc0947ef28d05
ixio0330 <ixio0330@gmail.com>
2023-10-08 10:02:46 +0900

    💄 Noto sans KR 폰트 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 'use client'
 
 import '@/styles/globals.css';
+import { Noto_Sans_KR } from 'next/font/google';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AuthUserProvider } from '@/client/context/auth_user';
 
+const noto_sans_KR = Noto_Sans_KR({ subsets: ['latin'] });
+
 const RootLayout = ({ children }: { children: React.ReactNode}) => (
   <html lang='ko'>
-    <body>
+    <body className={noto_sans_KR.className}>
       <ChakraProvider>
         <AuthUserProvider>
           {children}
