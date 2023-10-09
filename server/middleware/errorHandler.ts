f983a1c235ff3739262ca226201dc7c84eea9a63
ixio0330 <ixio0330@gmail.com>
2023-10-09 14:13:20 +0900

    🐛 ErrorHandler 상태 코드 누락

--- server/middleware/errorHandler.ts
+++ server/middleware/errorHandler.ts
@@ -13,7 +13,7 @@ export default errorHandler;
 
 export const responseErrorHandler = (error: unknown) => {
   if (error instanceof CustomError) {
-    return NextResponse.json({ message: error.message });
+    return NextResponse.json({ message: error.message }, { status: error.statusCode });
   }
-  return NextResponse.json({ message: '서버 내부 오류가 발생했습니다.' });
+  return NextResponse.json({ message: '서버 내부 오류가 발생했습니다.' }, { status: 500 });
 };
